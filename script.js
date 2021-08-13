bc03dbe923ee6c9d79ea630ba1cfdb355ff4b0a9
Pete Stern <p.stern@ix.netcom.com>
2021-08-12 17:17:30 -0700

    Removed colors, added images; corrected timing of flips and click count

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let bestScore = 0;
 
 // call a function handleCardClick when a div is clicked on
 startBtn.addEventListener("click", function () {
-  startBtn.remove();
-
+  startBtn.style.visibility = 'hidden';
   playGame();
 });
 
@@ -16,19 +15,28 @@ startBtn.addEventListener("click", function () {
 h1.after(startBtn);
 
 function playGame() {
-  const COLORS = [
-    "red",
-    "blue",
-    "green",
-    "orange",
-    "purple",
-    "yellow",
-    "red",
-    "blue",
-    "green",
-    "orange",
-    "purple",
-    "yellow"
+  const IMAGES = [
+    "arnold",
+    "bugs",
+    "dumbo",
+    "LzsS",
+    "mgmlion",
+    "mickey",
+    "pota",
+    "rabbit",
+    "tigger",
+    "toto",
+    //
+    "arnold",
+    "bugs",
+    "dumbo",
+    "LzsS",
+    "mgmlion",
+    "mickey",
+    "pota",
+    "rabbit",
+    "tigger",
+    "toto"
   ];
 
   let ctr = 0;
@@ -50,8 +58,8 @@ function playGame() {
     h3.innerHTML = `<h3>Best: <span id='best'>${lsScore}</span> Clicks: <span id='clicks'>0</span></h3>`;
   }
 
-  // append the h3 after the h1
-  h1.after(h3);
+  // append the h3 after the start button
+  startBtn.after(h3);
 
   // here is a helper function to shuffle an array
   // it returns the same array with values shuffled
@@ -76,19 +84,21 @@ function playGame() {
     return array;
   }
 
-  let shuffledColors = shuffle(COLORS);
+  let shuffledImages = shuffle(IMAGES);
 
-  // this function loops over the array of colors
-  // it creates a new div and gives it a class with the value of the color
+  // this function loops over the array of images
+  // it creates a new div and gives it a class with the value of the image
   // it also adds an event listener for a click for each card
-  function createDivsForColors(colorArray) {
+  function createDivsForImages(imageArray) {
     let count = 0;
-    for (let color of colorArray) {
+    for (let image of imageArray) {
       // create a new div
       const newDiv = document.createElement("div");
 
       // give it a class attribute for the value we are looping over
-      newDiv.classList.add(color);
+      newDiv.classList.add(image);
+
+      newDiv.style.backgroundColor = "white";
 
       // give it a unique id for the value we are looping over
       newDiv.id = count;
@@ -120,89 +130,94 @@ function playGame() {
     if ((!gameOver) && (!matchedIdsArray.includes(event.target.id)) && (!alreadyClicked)) {
       const clicksSpan = document.querySelector("#clicks");
 
-      clicks += 1;
-      clicksSpan.innerText = clicks;
-
       // Users should only be able to change at most two cards at a time => ctr < 2
       if (ctr < 2) {
-        div.style.backgroundColor = event.target.classList;
+        clicks += 1;
+        clicksSpan.innerText = clicks;
+
+        div.style.backgroundImage = `url(./images/${event.target.classList}.jpg)`;
+        div.style.backgroundSize = "cover";
 
         ctr += 1;
 
-        let timer = setTimeout(function () {
-          ctr -= 1;
-          div.style.backgroundColor = "";
-
-          for (let item of clickedArray) {
-            if (item.id === div.id) {
-              clickedArray.splice(item, 1);
-            }
-          }
-        }, 1000);
-
-        arrayObj = { color: event.target.classList.value, id: event.target.id, timerId: timer };
+        arrayObj = { image: event.target.classList.value, id: event.target.id };
         clickedArray.push(arrayObj);
 
-        if ((clickedArray.length === 2) && (clickedArray[0].color === clickedArray[1].color)) {
-          clearTimeout(clickedArray[0].timerId);
-          clearTimeout(clickedArray[1].timerId);
+        if (ctr > 1) {
+          const firstFlip = document.getElementById(clickedArray[0].id);
+          const secondFlip = document.getElementById(clickedArray[1].id);
 
-          const match0 = document.getElementById(clickedArray[0].id);
-          const match1 = document.getElementById(clickedArray[1].id);
-          matchedIdsArray.push(clickedArray[0].id);
-          matchedIdsArray.push(clickedArray[1].id);
+          // start timer after second click
+          let timerId = setTimeout(function () {
+            ctr -= 2;
+            firstFlip.style.backgroundImage = "";
+            firstFlip.style.backgroundColor = "white";
+            secondFlip.style.backgroundImage = "";
+            secondFlip.style.backgroundColor = "white";
+            clickedArray.splice(0, 2);
+          }, 1000);
 
-          match0.style.backgroundColor = event.target.classList;
-          match1.style.backgroundColor = event.target.classList;
+          // match logic
+          if (clickedArray[0].image === clickedArray[1].image) {
+            clearTimeout(timerId);
+            matchedIdsArray.push(clickedArray[0].id);
+            matchedIdsArray.push(clickedArray[1].id);
 
-          clickedArray.splice(0, 2);
-          ctr = 0;
-          noOfMatches += 1;
+            firstFlip.style.backgroundImage = `url(./images/${event.target.classList}.jpg)`;
+            firstFlip.style.backgroundSize = "cover";
+            secondFlip.style.backgroundImage = `url(./images/${event.target.classList}.jpg)`;
+            secondFlip.style.backgroundSize = "cover";
 
-          if (COLORS.length === noOfMatches * 2) {
-            gameOver = true;
+            clickedArray.splice(0, 2);
+            ctr = 0;
+            noOfMatches += 1;
 
-            const bestSpan = document.querySelector("#best");
-            if ((bestScore === 0) || (bestScore > clicks)) {
-              bestScore = clicks;
-              bestSpan.innerText = bestScore;
+            // game over logic
+            if (IMAGES.length === noOfMatches * 2) {
+              gameOver = true;
 
-              localStorage.clear();
-              localStorage.setItem("BestScore", JSON.stringify(bestScore));
-            }
-
-            // create restart button
-            const restartBtn = document.createElement("button");
-            restartBtn.classList.add("btn");
-            restartBtn.innerText = "Restart";
-
-            // give it a class attribute for the value we are looping over
-            // newDiv.classList.add(color);
-
-            // call a function handleCardClick when a div is clicked on
-            restartBtn.addEventListener("click", function () {
-
-              // select all color divs
               const divs = document.querySelectorAll("div:not(#game)");
 
               for (let div of divs) {
-                div.remove();
+                div.style.backgroundImage = `url(./images/${div.classList}.gif)`;
               }
-              // window.location.reload()
 
-              restartBtn.remove();
-              h3.remove();
-              playGame()
-            });
+              const bestSpan = document.querySelector("#best");
+              if ((bestScore === 0) || (bestScore > clicks)) {
+                bestScore = clicks;
+                bestSpan.innerText = bestScore;
 
-            // append the div to the element with an id of game
-            gameContainer.after(restartBtn);
+                localStorage.clear();
+                localStorage.setItem("BestScore", JSON.stringify(bestScore));
+              }
+
+              // create restart button
+              const restartBtn = document.createElement("button");
+              restartBtn.classList.add("btn");
+              restartBtn.innerText = "Restart";
+
+              restartBtn.addEventListener("click", function () {
+
+                // select all image divs
+                const divs = document.querySelectorAll("div:not(#game)");
+
+                for (let div of divs) {
+                  div.remove();
+                }
+                // window.location.reload()
+                restartBtn.remove();
+                h3.remove();
+                playGame()
+              });
+
+              // append the restart button where start button was
+              h1.after(restartBtn);
+            }
           }
         }
       }
     }
   }
-
   // when the DOM loads
-  createDivsForColors(shuffledColors);
+  createDivsForImages(shuffledImages);
 }
